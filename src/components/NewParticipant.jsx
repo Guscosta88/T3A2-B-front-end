ed616896105ab0592282328f1a356afb9d4326a3
Guscosta88 <guslfcosta@gmail.com>
2023-02-02 15:58:29 +1100

    NewParticipant component structure added

--- src/components/NewParticipant.jsx
+++ src/components/NewParticipant.jsx
@@ -3,8 +3,36 @@ import BeverageSelector from './BeverageSelector'
 
 const NewParticipant = () => {
   return (
-    <div>
+    <div class="card">
+        <h5>Add Participant</h5>
+        <form action="">
+        <label class="sr-only" for="inlineFormInput">Enter Name</label>
+            <div class="col-sm-10">
+            <input type="text" class="form-control" id="inlineFormInput" placeholder="Name"></input>
+      </div>
         <BeverageSelector />
+        <label class="sr-only" for="inlineFormInput">Meat eater?</label>
+            <div class="col-sm-10">
+            <input 
+                class="form-check-input" 
+                type="radio" 
+                name="is_meat_eater" 
+                id="yes" 
+                value="yes">
+            </input>
+
+        <label class="form-check-label" for="yes">Yes</label>
+            <input 
+                class="form-check-input" 
+                type="radio" 
+                name="is_meat_eater" 
+                id="no" 
+                value="no">
+            </input>
+        <label class="form-check-label" for="no">No</label>
+            </div>
+        <button className="btn btn-secondary mt-2">Submit</button>
+        </form>
     </div>
   )
 }
