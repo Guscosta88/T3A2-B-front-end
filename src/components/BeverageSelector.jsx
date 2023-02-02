8ef5d692952439299119e94d6927b93402310395
Guscosta88 <guslfcosta@gmail.com>
2023-02-02 15:48:09 +1100

    BeverageSelector Component added and fetching beverages from the API

--- src/components/BeverageSelector.jsx
+++ src/components/BeverageSelector.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
-const BeverageSelector = () => {
+const BeverageSelector = ( { beverage, setBeverage } ) => {
+    let [beverages, setBeverages] = useState([])
+
+    useEffect(() => {
+        fetch("https://t3a2-b-back-end-production.up.railway.app/beverages")
+        .then((res) => res.json())
+        .then((data) => setBeverages(data))
+    }, [])
+
   return (
-    <div>BeverageSelector</div>
+    <div>
+        <label> Beverage </label><br />
+
+        <select onChange={(evt) => setBeverage(evt.target.value)} value={beverage}>
+
+            {beverages.map(bev => (
+                <option key={bev.beverage} value={bev.beverage}>
+                    {bev.name}
+                </option>
+            ))}
+        </select>
+    </div>
   )
 }
 
--- src/components/NewParticipant.jsx
+++ src/components/NewParticipant.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import BeverageSelector from './BeverageSelector'
 
 const NewParticipant = () => {
   return (
-    <div>NewParticipant</div>
+    <div>
+        <BeverageSelector />
+    </div>
   )
 }
 
